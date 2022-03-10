8548db0274d568f9a25c473142b6e0e4a9e57b4f
est5 <ghostrekon5500@ya.ru>
2022-03-10 14:31:39 +0300

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const DisplayHeader = ({ text }) => <h1>{text}</h1>;
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-const DisplayPara = ({ text, value }) => (
+const DisplayStat = ({ text, value }) => (
   <p>
     {text} {value}
   </p>
@@ -15,6 +15,18 @@ const App = () => {
     return () => setStats({ ...stats, [stat]: stats[stat] + 1 });
   };
 
+  const calculateAll = () => {
+    return stats.good + stats.neutral + stats.bad;
+  };
+
+  const calculateAvg = () => {
+    return (stats.good - stats.bad) / calculateAll();
+  };
+
+  const calculatePos = () => {
+    return (stats.good / calculateAll()) * 100 + ' %';
+  };
+
   return (
     <div>
       <DisplayHeader text="give feedback" />
@@ -22,9 +34,12 @@ const App = () => {
       <Button onClick={incrementStat('neutral')} text="neutral" />
       <Button onClick={incrementStat('bad')} text="bad" />
       <DisplayHeader text="statistics" />
-      <DisplayPara text={'good'} value={stats.good} />
-      <DisplayPara text={'neutral'} value={stats.neutral} />
-      <DisplayPara text={'bad'} value={stats.bad} />
+      <DisplayStat text={'good'} value={stats.good} />
+      <DisplayStat text={'neutral'} value={stats.neutral} />
+      <DisplayStat text={'bad'} value={stats.bad} />
+      <DisplayStat text="all" value={calculateAll()} />
+      <DisplayStat text="average" value={calculateAvg()} />
+      <DisplayStat text="positive" value={calculatePos()} />
     </div>
   );
 };
